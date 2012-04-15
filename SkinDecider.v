72b3dfbd23017eb45ef14f286912d2acf816caa3
zEko Frost <zeko.frost@gmail.com>
2012-04-15 14:34:49 +0530

    All skin_pix references has been changed to object_image, for consistency

--- SkinDecider.v
+++ SkinDecider.v
@@ -1,24 +1,24 @@
 module SkinDecider(luma_ch, cb_ch, cr_ch,
-		   skin_pix,
+		   object_image,
 		   rst, clk);
 
    input [7:0] luma_ch, cb_ch, cr_ch;
-   output      skin_pix;
+   output      object_image;
 
    input 	rst, clk;
 
-   reg  	skin_pix;
+   reg  	object_image;
 
    always @(posedge clk) begin
       if (rst)
-	 skin_pix = 0;
+	 object_image = 0;
       else begin
 	if ((luma_ch > 80) &&
 	    (cb_ch > 125) && (cb_ch < 180) && 
 	    (cr_ch > 190) && (cr_ch < 225))
-	  skin_pix = 1;
+	  object_image = 1;
 	else
-	  skin_pix = 0;
+	  object_image = 0;
       end
    end // always @ (posedge clk)
 endmodule // SkinDecider
