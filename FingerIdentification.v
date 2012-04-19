2403e438306a1b8657bb84a3f012bc2441c3bfda
zEko Frost <zeko.frost@gmail.com>
2012-04-19 19:59:16 +0530

    blocking to non blocking, and keeping count of row and columns

--- FingerIdentification.v
+++ FingerIdentification.v
@@ -20,21 +20,32 @@ module FingerIdentification(palm_width,
    input       rst, clk;
 
    reg      thumb_status, index_status, middle_status, ring_status, pinky_status;
-
+   
    always @(posedge clk) begin
       if(rst) begin
-	 thumb_status = 0;
-	 index_status = 0;
-	 middle_status = 0;
-	 ring_status = 0;
-	 pinky_status = 0;
-     end
-
-     else begin
-	if (palm_width != 0) begin
-	   // Calculate the status of each finger
-	end
-     end // else: !if(rst)
+	 thumb_status <= 0;
+	 index_status <= 0;
+	 middle_status <= 0;
+	 ring_status <= 0;
+	 pinky_status <= 0;
+      end
+      
+      else begin
+	 // The palm has been found, start calculating the fingers
+	 if (palm_width != 0) begin
+	    // Meanwhile keep track of the image row and columns
+	    if(col_count == IMAGE_WIDTH) begin
+	       // columns should not exceed image_width
+	       col_count <= 0;
+	       // increment row after 1 scan of column
+	       row_count <= row_count + 1;
+	    end
+	    else begin
+   	       col_count <= col_count + 1;
+	    end
+	    // Calculate the status of each finger
+	 end
+      end // else: !if(rst)
    end // always @ posedge(clk)
 endmodule // FingerIdentification
 
